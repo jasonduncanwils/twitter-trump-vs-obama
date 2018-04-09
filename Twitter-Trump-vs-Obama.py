264210d2b960a17e67991eb438a02dec601a2fcf
jasonduncanwilson <jason@duncanwilsongroup.com>
2018-04-08 20:16:10 -0400

    Renamed file

--- Twitter-Trump-vs-Obama.py
+++ Twitter-Trump-vs-Obama.py
@@ -12,7 +12,6 @@ from nltk.corpus import stopwords
 Functions
 ------------------------------------------------------'''
 
-
 def strip_non_ascii(string):
     # Returns the string without non ASCII characters
     stripped = (c for c in string if 0 < ord(c) < 127)
